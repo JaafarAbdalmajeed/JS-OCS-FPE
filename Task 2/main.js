cf374acd6825b84142fa7205d0ed79124b3e9155
JaafarAlwahsh <jaafaralwahsh22@gmail.com>
2023-11-05 18:38:07 +0300

    done

--- Task 2/main.js
+++ Task 2/main.js
@@ -2,11 +2,11 @@ const mark = prompt("What is your mark in the material?")
 const result = document.getElementById("result")
 
 switch (true) {
-    case mark >= 90: result.innerHTML = 'A+';break;
-    case mark >= 80: result.innerHTML = 'A';break;
-    case mark >= 70: result.innerHTML = 'B';break;
-    case mark >= 60: result.innerHTML = 'C';break;
-    case mark >= 50: result.innerHTML = 'D';break;
+    case mark >= 90: document.write('<h2>A+</h2>');break;
+    case mark >= 80: document.write('<h2>A</h2>');break;
+    case mark >= 70: document.write('<h2>B</h2>');break;
+    case mark >= 60: document.write('<h2>AC</h2>');break;
+    case mark >= 50: document.write('<h2>AD</h2>');break;
     default: result.innerHTML = 'F';
 }
 
