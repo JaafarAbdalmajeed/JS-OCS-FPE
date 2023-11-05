df60e687603dd5565cdd8f956379cc1405b6631e
JaafarAlwahsh <jaafaralwahsh22@gmail.com>
2023-11-05 18:59:52 +0300

    task 4 it is done

---
+++ Task 4/main.js
@@ -0,0 +1,12 @@
+let x = prompt("x:")
+let y = prompt("y:")
+
+if (x > y) {
+    console.log("greater")
+    document.write("<h2>greater</h2>")
+    alert("greater")
+} else {
+    console.log("Goodbye")
+    document.write("<h2>Goodbye</h2>")
+    alert("Goodbye")
+}
